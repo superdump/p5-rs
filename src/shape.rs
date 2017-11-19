33e5d83a96cc711060b51ba94fd86253e6c26882
Robert Swain <robert.swain@gmail.com>
2017-11-19 16:38:41 +0100

    shape: Support GL drawing shapes from main thread

--- src/shape.rs
+++ src/shape.rs
@@ -1,5 +1,7 @@
+use channel;
 use point::*;
 use shader::*;
+use sketch::SKETCH;
 
 use gl::types::*;
 use gl;
@@ -16,10 +18,11 @@ pub trait Shape {
 }
 
 fn point_to_vertex(point: Point) -> [GLfloat; 3] {
+    let sketch = SKETCH.lock().unwrap();
     [
-        point.x as GLfloat / 640.0 as GLfloat,
-        point.y as GLfloat / 360.0 as GLfloat,
-        point.z as GLfloat / 360.0 as GLfloat // FIXME: think about how to convert z
+        point.x as GLfloat / sketch.width as GLfloat,
+        point.y as GLfloat / sketch.height as GLfloat,
+        point.z as GLfloat / sketch.height as GLfloat // FIXME: think about how to convert z
     ]
 }
 
@@ -43,51 +46,51 @@ pub fn draw(shape: &Shape) {
     let vertex_shader_src = shape.vertex_shader();
     let fragment_shader_src = shape.fragment_shader();
 
-    let mut vao = 0;
-    let mut vbo = 0;
-    let mut ebo = 0;
-    let vertex_shader = compile_shader(&vertex_shader_src, gl::VERTEX_SHADER);
-    let fragment_shader = compile_shader(&fragment_shader_src, gl::FRAGMENT_SHADER);
-    let shader_program = link_program(vertex_shader, fragment_shader);
+    channel::send_closure(Box::new(move || {
+        let mut vao = 0;
+        let mut vbo = 0;
+        let mut ebo = 0;
+        let vertex_shader = compile_shader(&vertex_shader_src, gl::VERTEX_SHADER);
+        let fragment_shader = compile_shader(&fragment_shader_src, gl::FRAGMENT_SHADER);
+        let shader_program = link_program(vertex_shader, fragment_shader);
 
-    unsafe {
-        gl::GenVertexArrays(1, &mut vao);
-        gl::BindVertexArray(vao);
+        unsafe {
+            gl::GenVertexArrays(1, &mut vao);
+            gl::BindVertexArray(vao);
 
-        gl::GenBuffers(1, &mut vbo);
-        gl::BindBuffer(gl::ARRAY_BUFFER, vbo);
-        gl::BufferData(
-            gl::ARRAY_BUFFER,
-            (vertex_data.len() * mem::size_of::<GLfloat>()) as GLsizeiptr,
-            mem::transmute(&vertex_data[0]),
-            gl::STATIC_DRAW,
-        );
+            gl::GenBuffers(1, &mut vbo);
+            gl::BindBuffer(gl::ARRAY_BUFFER, vbo);
+            gl::BufferData(
+                gl::ARRAY_BUFFER,
+                (vertex_data.len() * mem::size_of::<GLfloat>()) as GLsizeiptr,
+                mem::transmute(&vertex_data[0]),
+                gl::STATIC_DRAW,
+            );
 
-        gl::GenBuffers(1, &mut ebo);
-        gl::BindBuffer(gl::ELEMENT_ARRAY_BUFFER, ebo);
-        gl::BufferData(
-            gl::ELEMENT_ARRAY_BUFFER,
-            (index_data.len() * mem::size_of::<GLuint>()) as GLsizeiptr,
-            mem::transmute(&index_data[0]),
-            gl::STATIC_DRAW,
-        );
+            gl::GenBuffers(1, &mut ebo);
+            gl::BindBuffer(gl::ELEMENT_ARRAY_BUFFER, ebo);
+            gl::BufferData(
+                gl::ELEMENT_ARRAY_BUFFER,
+                (index_data.len() * mem::size_of::<GLuint>()) as GLsizeiptr,
+                mem::transmute(&index_data[0]),
+                gl::STATIC_DRAW,
+            );
 
-        // Specify the layout of the vertex data
-        let pos_attr = gl::GetAttribLocation(shader_program, CString::new("position").unwrap().as_ptr());
-        gl::VertexAttribPointer(
-            pos_attr as GLuint,
-            3,
-            gl::FLOAT,
-            gl::FALSE as GLboolean,
-            3 * mem::size_of::<GLfloat>() as GLint,
-            ptr::null(),
-        );
-        gl::EnableVertexAttribArray(pos_attr as GLuint);
-    }
+            // Specify the layout of the vertex data
+            let pos_attr = gl::GetAttribLocation(shader_program, CString::new("position").unwrap().as_ptr());
+            gl::VertexAttribPointer(
+                pos_attr as GLuint,
+                3,
+                gl::FLOAT,
+                gl::FALSE as GLboolean,
+                3 * mem::size_of::<GLfloat>() as GLint,
+                ptr::null(),
+            );
+            gl::EnableVertexAttribArray(pos_attr as GLuint);
 
-    unsafe {
-        gl::UseProgram(shader_program);
-        gl::BindVertexArray(vao);
-        gl::DrawElements(gl::TRIANGLES, 3, gl::UNSIGNED_INT, ptr::null());
-    }
+            gl::UseProgram(shader_program);
+            gl::BindVertexArray(vao);
+            gl::DrawElements(gl::TRIANGLES, 3, gl::UNSIGNED_INT, ptr::null());
+        }
+    }));
 }
