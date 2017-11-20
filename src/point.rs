7f9a9c81b12c73c9544715e7525039de22d9fee7
Robert Swain <robert.swain@gmail.com>
2017-11-20 10:00:51 +0100

    point: Support addition and conversion from tuple

--- src/point.rs
+++ src/point.rs
@@ -1,16 +1,73 @@
-#[derive(Clone)]
+use std::ops::Add;
+
+#[derive(Copy, Clone)]
 pub struct Point {
     pub x: f32,
     pub y: f32,
-    pub z: f32
+    pub z: f32,
 }
 
 impl Point {
     pub fn new(x: f32, y: f32, z: f32) -> Point {
+        Point { x: x, y: y, z: z }
+    }
+}
+
+impl From<(f32, f32, f32)> for Point {
+    fn from(p: (f32, f32, f32)) -> Self {
         Point {
-            x: x,
-            y: y,
-            z: z
+            x: p.0,
+            y: p.1,
+            z: p.2,
+        }
+    }
+}
+
+impl<'a, 'b> Add<&'b Point> for &'a Point {
+    type Output = Point;
+
+    fn add(self, other: &'b Point) -> Point {
+        Point {
+            x: self.x + other.x,
+            y: self.y + other.y,
+            z: self.z + other.z,
+        }
+    }
+}
+
+impl<'a> Add<&'a Point> for Point {
+    type Output = Point;
+
+    fn add(self, other: &'a Point) -> Point {
+        Point {
+            x: self.x + other.x,
+            y: self.y + other.y,
+            z: self.z + other.z,
+        }
+    }
+}
+
+
+impl<'a> Add<Point> for &'a Point {
+    type Output = Point;
+
+    fn add(self, other: Point) -> Point {
+        Point {
+            x: self.x + other.x,
+            y: self.y + other.y,
+            z: self.z + other.z,
+        }
+    }
+}
+
+impl Add for Point {
+    type Output = Point;
+
+    fn add(self, other: Point) -> Point {
+        Point {
+            x: self.x + other.x,
+            y: self.y + other.y,
+            z: self.z + other.z,
         }
     }
 }
