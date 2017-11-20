eaf4fb438464c9bf684fdd704f85c2c3c2d38f47
Robert Swain <robert.swain@gmail.com>
2017-11-20 10:01:06 +0100

    triangle: Create from tuple

--- src/triangle.rs
+++ src/triangle.rs
@@ -3,8 +3,8 @@ use shader;
 use shape;
 use shape::Shape;
 
-pub fn triangle(p1: Point, p2: Point, p3: Point) {
-    Triangle::new(p1, p2, p3).draw();
+pub fn triangle<P: Into<Point>>(p1: P, p2: P, p3: P) {
+    Triangle::new(p1.into(), p2.into(), p3.into()).draw();
 }
 
 pub struct Triangle {
