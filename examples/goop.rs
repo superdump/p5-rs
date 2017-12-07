2ae4eaa94cf884b477b36bce52fb151ebffd6070
Robert Swain <robert.swain@gmail.com>
2017-12-07 11:01:35 +0100

    examples/goop: Tweak individual values

--- examples/goop.rs
+++ examples/goop.rs
@@ -31,7 +31,6 @@
 extern crate noise;
 extern crate p5;
 
-use noise::{NoiseModule, Perlin};
 use p5::*;
 
 use std::f32;
@@ -43,13 +42,7 @@ const N_POINTS: usize = 10_000;
 static mut POINTS: Option<Vec<Point3<f32>>> = None;
 static mut T: f32 = 0.0;
 
-static mut PERLIN: Option<Perlin> = None;
-
 fn setup() {
-    unsafe {
-        PERLIN = Some(Perlin::new());
-    }
-
     size(WIDTH, HEIGHT);
     background(0.2);
 
@@ -64,20 +57,15 @@ fn setup() {
     }
 }
 
-const SCALE: f32 = 0.007;
 const RADIUS: f32 = 1.0;
 
 fn draw() {
     stroke((1.0, 1.0, 1.0, 0.3));
     stroke_weight(1);
 
-    let t;
-    unsafe {
-        t = T;
-        T += 1.0 / 500.0;
-    }
-
     // 1D - 0.01
+    // stroke_weight(2);
+    // const SCALE: f32 = 0.03;
     // for x in 0..WIDTH {
     //     let y = HEIGHT as f32 * noise(Point4::new(
     //         SCALE * x as f32,
@@ -93,6 +81,8 @@ fn draw() {
     // }
 
     // 2D
+    // stroke_weight(1);
+    // const SCALE: f32 = 0.01;
     // for y in 0..HEIGHT {
     //     for x in 0..WIDTH {
     //         let color = noise(Point4::new(
@@ -110,9 +100,15 @@ fn draw() {
     // }
 
     // 2D + time
-    // stroke_weight(4);
-    // for y in 0..HEIGHT/4 {
-    //     for x in 0..WIDTH/4 {
+    // let t;
+    // unsafe {
+    //     t = T;
+    //     T += 1.0 / 75.0;
+    // }
+    // stroke_weight(1);
+    // const SCALE: f32 = 0.01;
+    // for y in 0..HEIGHT {
+    //     for x in 0..WIDTH {
     //         let color = noise(Point4::new(
     //             x as f32 * SCALE,
     //             y as f32 * SCALE,
@@ -120,8 +116,8 @@ fn draw() {
     //         ));
     //         stroke(color);
     //         point(Point3::new(
-    //             4.0 * (x as f32 - 0.5 * (WIDTH / 4) as f32),
-    //             4.0 * (y as f32 - 0.5 * (HEIGHT / 4) as f32),
+    //             x as f32 - 0.5 * WIDTH as f32,
+    //             y as f32 - 0.5 * HEIGHT as f32,
     //             0.0,
     //         ));
     //     }
@@ -129,18 +125,14 @@ fn draw() {
 
     // 2D with perlin and thresholding
     // stroke_weight(4);
+    // const SCALE: f32 = 0.05;
     // for y in 0..HEIGHT/4 {
     //     for x in 0..WIDTH/4 {
-    //         let mut n = 0.0;
-    //         unsafe {
-    //             if let Some(perlin) = PERLIN {
-    //                 n = perlin.get([
-    //                     x as f32 * SCALE,
-    //                     y as f32 * SCALE,
-    //                     0.0, 0.0,
-    //                 ]);
-    //             }
-    //         }
+    //         let n = noise(Point4::new(
+    //             x as f32 * SCALE,
+    //             y as f32 * SCALE,
+    //             0.0, 0.0,
+    //         ));
     //         let mut c = 1.0;
     //         if n > 0.5 {
     //             c = 0.0;
@@ -154,24 +146,24 @@ fn draw() {
     //     }
     // }
 
-    let angle = 2.0 * f32::consts::PI * t;
-    let rcos = RADIUS * angle.cos();
-    let rsin = RADIUS * angle.sin();
-
     // 2D + time with perlin and thresholding
+    // let t;
+    // unsafe {
+    //     t = T;
+    //     T += 1.0 / 300.0;
+    // }
     // stroke_weight(4);
+    // const SCALE: f32 = 0.04;
+    // let angle = 2.0 * f32::consts::PI * t;
+    // let rcos = RADIUS * angle.cos();
+    // let rsin = RADIUS * angle.sin();
     // for y in 0..HEIGHT/4 {
     //     for x in 0..WIDTH/4 {
-    //         let mut n = 0.0;
-    //         unsafe {
-    //             if let Some(perlin) = PERLIN {
-    //                 n = perlin.get([
-    //                     x as f32 * SCALE,
-    //                     y as f32 * SCALE,
-    //                     rcos, rsin,
-    //                 ]);
-    //             }
-    //         }
+    //         let n = noise(Point4::new(
+    //             x as f32 * SCALE,
+    //             y as f32 * SCALE,
+    //             rcos, rsin,
+    //         ));
     //         let mut c = 1.0;
     //         if n > 0.5 {
     //             c = 0.0;
@@ -186,19 +178,22 @@ fn draw() {
     // }
 
     // 1D + time with perlin
+    // let t;
+    // unsafe {
+    //     t = T;
+    //     T += 1.0 / 300.0;
+    // }
+    // stroke_weight(1);
+    // const SCALE: f32 = 0.01;
+    // let angle = 2.0 * f32::consts::PI * t;
+    // let rcos = RADIUS * angle.cos();
+    // let rsin = RADIUS * angle.sin();
     // stroke_weight(4);
     // for x in 0..WIDTH {
-    //     let mut n = 0.0;
-    //     unsafe {
-    //         if let Some(perlin) = PERLIN {
-    //             n = perlin.get([
-    //                 x as f32 * SCALE,
-    //                 rcos,
-    //                 rsin,
-    //                 0.0,
-    //             ]);
-    //         }
-    //     }
+    //     let n = 2.0 * (noise(Point4::new(
+    //         x as f32 * SCALE,
+    //         rcos, rsin, 0.0,
+    //     )) - 0.5);
     //     let y = HEIGHT as f32 * n;
     //     point(Point3::new(
     //         x as f32 - 0.5 * WIDTH as f32,
@@ -208,19 +203,23 @@ fn draw() {
     // }
 
     // 2D + time with perlin
+    // let t;
+    // unsafe {
+    //     t = T;
+    //     T += 1.0 / 300.0;
+    // }
     // stroke_weight(4);
+    // const SCALE: f32 = 0.05;
+    // let angle = 2.0 * f32::consts::PI * t;
+    // let rcos = RADIUS * angle.cos();
+    // let rsin = RADIUS * angle.sin();
     // for y in 0..HEIGHT/4 {
     //     for x in 0..WIDTH/4 {
-    //         let mut color = 0.0;
-    //         unsafe {
-    //             if let Some(perlin) = PERLIN {
-    //                 color = (perlin.get([
-    //                     x as f32 * SCALE,
-    //                     y as f32 * SCALE,
-    //                     rcos, rsin,
-    //                 ]) + 1.0) * 0.5;
-    //             }
-    //         }
+    //         let color = noise(Point4::new(
+    //             x as f32 * SCALE,
+    //             y as f32 * SCALE,
+    //             rcos, rsin,
+    //         ));
     //         stroke(color);
     //         point(Point3::new(
     //             4.0 * (x as f32 - 0.5 * (WIDTH / 4) as f32),
@@ -230,27 +229,34 @@ fn draw() {
     //     }
     // }
 
+    let t;
+    unsafe {
+        t = T;
+        T += 1.0 / 300.0;
+    }
     stroke_weight(2);
+    const SCALE: f32 = 0.005;
+    let angle = 2.0 * f32::consts::PI * t;
+    let rcos = RADIUS * angle.cos();
+    let rsin = RADIUS * angle.sin();
     unsafe {
         if let Some(ref points) = POINTS {
-            if let Some(perlin) = PERLIN {
-                for p in points {
-                    let intensity = 100.0
-                        * map_f32(
-                            distance(p, &Point3::origin()),
-                            0.0,
-                            WIDTH as f32 * 0.5,
-                            1.0,
-                            0.0,
-                        );
-                    point(Point3::new(
-                        p.x + intensity * perlin.get([p.x * SCALE, p.y * SCALE, rcos, rsin]),
-                        p.y
-                            + intensity
-                                * perlin.get([100.0 + p.x * SCALE, p.y * SCALE, rcos, rsin]),
+            for p in points {
+                let intensity = 250.0
+                    * map_f32(
+                        distance(p, &Point3::origin()),
                         0.0,
-                    ));
-                }
+                        WIDTH as f32 * 0.5,
+                        1.0,
+                        0.0,
+                    );
+                point(Point3::new(
+                    p.x + intensity * (noise(Point4::new(p.x * SCALE, p.y * SCALE, rcos, rsin)) - 0.5),
+                    p.y
+                        + intensity
+                            * (noise(Point4::new(100.0 + p.x * SCALE, p.y * SCALE, rcos, rsin)) - 0.5),
+                    0.0,
+                ));
             }
         }
     }
